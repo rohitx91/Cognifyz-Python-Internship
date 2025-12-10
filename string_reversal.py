853f641762ade2bcdb4f5b7306263e493fc739c1
Rohit Pandit <panditrohitbr@gmail.com>
2025-12-10 09:44:01 +0530

    Includes Python code, output screenshot, and project documentation for the String Reversal task.

---
+++ string_reversal.py
@@ -0,0 +1,11 @@
+def reverse_string(text):
+    return text[::-1]
+
+# Taking input from user
+user_input = input("Enter a string to reverse: ")
+
+# Calling the function and printing output
+reversed_text = reverse_string(user_input)
+print("Reversed string:", reversed_text)
+
+
